39d1e4b82a6835a7e250f4c175171c53b5922208
giangnd <giang.ndj.hlg@gmail.com>
2024-04-20 15:20:59 +0700

    add common user token

---
+++ src/main/java/vtp/crm/common/vo/notication/FcmTokensByUsersResponse.java
@@ -0,0 +1,17 @@
+package vtp.crm.common.vo.notication;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class FcmTokensByUsersResponse {
+
+    private Long userId;
+
+    private Boolean isInternal;
+
+    private String phone;
+
+    private List<String> fcmTokens;
+}
